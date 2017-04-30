90c4f7a4656a1222cd9d15ae64e09ad3db2a8a05
roh-7 <rohit.6233@outlook.com>
2017-04-30 14:52:05 +0530

    splash

---
+++ app/src/main/java/com/example/shimmer_card_try/activities/Main.java
@@ -0,0 +1,15 @@
+package com.example.shimmer_card_try.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shimmer_card_try.R;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/shimmer_card_try/activities/Splash.java
+++ app/src/main/java/com/example/shimmer_card_try/activities/Splash.java
@@ -1,15 +1,26 @@
 package com.example.shimmer_card_try.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.shimmer_card_try.R;
 
 public class Splash extends AppCompatActivity {
 
+    private final int SPLASH_TIME_OUT=2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,Main.class));
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
