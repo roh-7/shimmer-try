ac9ececec3f362bf4f8fba68806d1eaafa7aaf94
roh-7 <rohit.6233@outlook.com>
2017-04-30 15:14:02 +0530

    shimmer initialised

--- app/src/main/java/com/example/shimmer_card_try/activities/Main.java
+++ app/src/main/java/com/example/shimmer_card_try/activities/Main.java
@@ -1,9 +1,10 @@
 package com.example.shimmer_card_try.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.shimmer_card_try.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class Main extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ShimmerFrameLayout shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer_frame);
+        shimmerFrameLayout.useDefaults();
+        shimmerFrameLayout.startShimmerAnimation();
     }
 }
