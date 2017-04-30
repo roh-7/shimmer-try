bc840a6b547214f1da6ff7b1cba06c1de76f725d
roh-7 <rohit.6233@outlook.com>
2017-04-30 15:22:15 +0530

    tp

--- app/src/main/java/com/example/shimmer_card_try/activities/Main.java
+++ app/src/main/java/com/example/shimmer_card_try/activities/Main.java
@@ -1,6 +1,7 @@
 package com.example.shimmer_card_try.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.shimmer_card_try.R;
@@ -12,8 +13,11 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Shimmer");
         final ShimmerFrameLayout shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer_frame);
         shimmerFrameLayout.useDefaults();
         shimmerFrameLayout.startShimmerAnimation();
+
     }
 }
