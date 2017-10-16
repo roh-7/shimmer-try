35f6e0d4d900cbd81e6d91bd722049938d5332fc
U-DESKTOP-AUMAJUM\Nick <blottn@tcd.ie>
2017-10-16 15:36:59 +0100

    added references to views to abstract some view finding logic away

--- app/src/main/java/com/example/shimmer_card_try/activities/Main.java
+++ app/src/main/java/com/example/shimmer_card_try/activities/Main.java
@@ -1,14 +1,21 @@
 package com.example.shimmer_card_try.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.shimmer_card_try.R;
 import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class Main extends AppCompatActivity
 {
+
+    private ImageView top_image, top_title, top_subtitle1, top_subtitle2, top_subtitle3;
+    private ImageView bottom_image, bottom_title, bottom_subtitle1, bottom_subtitle2, bottom_subtitle3;
+
+    private ShimmerFrameLayout top_shimmerFrameLayout, bottom_shimmerFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,15 +24,15 @@ public class Main extends AppCompatActivity
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Shimmer");
-        final ShimmerFrameLayout shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer1);
-        shimmerFrameLayout.setDuration(1500);
-        shimmerFrameLayout.setTilt(180);
-        shimmerFrameLayout.startShimmerAnimation();
+        final ShimmerFrameLayout top_shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer1);
+        top_shimmerFrameLayout.setDuration(1500);
+        top_shimmerFrameLayout.setTilt(180);
+        top_shimmerFrameLayout.startShimmerAnimation();
 
-        final ShimmerFrameLayout shimmerFrameLayout2 = (ShimmerFrameLayout) findViewById(R.id.shimmer2);
-        shimmerFrameLayout2.setDuration(1500);
-        shimmerFrameLayout2.setTilt(180);
-        shimmerFrameLayout2.startShimmerAnimation();
+        final ShimmerFrameLayout bottom_shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer2);
+        bottom_shimmerFrameLayout.setDuration(1500);
+        bottom_shimmerFrameLayout.setTilt(180);
+        bottom_shimmerFrameLayout.startShimmerAnimation();
 
     }
 }
